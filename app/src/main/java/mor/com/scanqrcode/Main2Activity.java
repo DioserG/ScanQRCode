2653d48e2a3e8edbea3eb1cb572b52807b1d1433
DioserG <54085379+DioserG@users.noreply.github.com>
2020-06-19 10:24:19 -0300

    Criação aba login, menu e Scan QRCode

--- app/src/main/java/mor/com/scanqrcode/Main2Activity.java
+++ app/src/main/java/mor/com/scanqrcode/Main2Activity.java
@@ -12,13 +12,18 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
+    public void btnScan(View view) {
+        Intent intent  = new Intent(Main2Activity.this,
+                Main3Activity.class);
+        startActivity(intent);
+    }
 
 }
