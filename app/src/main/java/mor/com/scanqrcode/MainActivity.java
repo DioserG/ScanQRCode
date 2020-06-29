3e2ef5c80610d6cb126600cf9f98a71a5363a061
DioserG <54085379+DioserG@users.noreply.github.com>
2020-06-29 10:05:38 -0300

    Criação aba login, menu e Scan QRCode

--- app/src/main/java/mor/com/scanqrcode/MainActivity.java
+++ app/src/main/java/mor/com/scanqrcode/MainActivity.java
@@ -7,15 +7,32 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity
 {
-            EditText user_name;
+            String urlWebServicesDesenvolvimento = "http://192.168.2.42:80/sucata/validaLogin.php";
+            String urlWebServicesProducao = "";
+
+            StringRequest stringRequest; //validação login
+            RequestQueue requestQueue;   //validação login
+
+            EditText user_name, editLogin, editSenha;
             Button btnLogar;
 
     @Override
@@ -24,6 +41,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestQueue = Volley.newRequestQueue(this); // necessário para validação login
+
+        editLogin = findViewById(R.id.user_name);
+        editSenha = findViewById(R.id.user_pass);
         btnLogar  = (Button) findViewById(R.id.btnLogar);
         btnLogar.setOnClickListener(new View.OnClickListener()//validações ao clicar no botão LOGAR
         {
@@ -31,27 +52,29 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view)
             {
                     boolean validade = true;
-                    TextView tLogin = (TextView) findViewById(R.id.user_name);
-                    TextView tSenha = (TextView) findViewById(R.id.user_pass);
-                    String login = tLogin.getText().toString();
-                    String senha = tSenha.getText().toString();
+                    //TextView tLogin = (TextView) findViewById(R.id.user_name);
+                    //TextView tSenha = (TextView) findViewById(R.id.user_pass);
+                    //String login = tLogin.getText().toString();
+                    //String senha = tSenha.getText().toString();
 
-                if(tLogin.getText().length()==0)// validando se usuário foi digitado
+                if(editLogin.getText().length()==0)// validando se usuário foi digitado
                 {
-                    tLogin.setError("Campo Obrigatório");
-                    tLogin.requestFocus();
+                    editLogin.setError("Campo Obrigatório");
+                    editLogin.requestFocus();
                     validade = false;
                 }
-                if(tSenha.getText().length()==0)// validando se senha foi digitado
+                if(editSenha.getText().length()==0)// validando se senha foi digitado
                 {
-                    tLogin.setError("Campo Obrigatório");
-                    tLogin.requestFocus();
+                    editSenha.setError("Campo Obrigatório");
+                    editSenha.requestFocus();
                     validade = false;
                 }
                 if(validade)
                 {
                     Toast.makeText(getApplicationContext(),"Validando seus dados ...espere",Toast.LENGTH_LONG).show();
-                }
+
+                    validarLogin();
+                }/*
                 if(login.equals("dioser") && senha.equals("123"))
                 {
                     alert("Login realizado com sucesso");
@@ -64,10 +87,43 @@ public class MainActivity extends AppCompatActivity
                 }else
                 {
                     alert("Login ou senha incorretos");
-                }
+                }*/
             }
         });//FIM validações botão LOGAR
     }
+
+
+    private void validarLogin()
+    {
+        stringRequest = new StringRequest(Request.Method.POST, urlWebServicesDesenvolvimento, new Response.Listener<String>()
+        {
+            @Override
+            public void onResponse(String response)
+            {
+                Log.v("LogLogin", response);
+                
+            }
+        }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Log.e("LogLogin", error.getMessage());
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<>();
+                params.put("login", editLogin.getText().toString());
+                params.put("senha", editSenha.getText().toString());
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
 
     private void alert (String s)
     {
